71493f20db944f005f60868192e79e6b4f3d2110
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-02 00:01:05 -0500

    add openweather api url and fetchWeather method

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = 'd171f5bc8a9b1c3383a9479b3051f447';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
