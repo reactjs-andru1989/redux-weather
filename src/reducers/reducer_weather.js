7d88bb344797091935467e22f00f5b1d272376b5
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-09 00:15:55 -0500

    change comment

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return [ action.payload.data, ...state ]; // it creates a new Array
+      return [ action.payload.data, ...state ]; // it creates a new Array => [city, city, city]
   }
   return state;
 }
