4325d3011abe33aee647d8023c24cec856303831
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-09 00:17:36 -0500

    change searchbar state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
     this.onInputChange = this.onInputChange.bind(this); // change the this context for onInputChange method
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -14,6 +18,9 @@ export default class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault();
     // We need to go and fetch weather data
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
@@ -32,3 +39,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
